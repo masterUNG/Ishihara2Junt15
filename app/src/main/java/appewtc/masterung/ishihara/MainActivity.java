439344718e15c9e045bf92c55702b26b1c63bb75
masterUNG <phrombutr@gmail.com>
2015-06-02 15:57:31 +0700

    Sound Effect

--- app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.ishihara;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -41,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
         answerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                buttonPlayer.start();
+
             }   // event
         });
 
